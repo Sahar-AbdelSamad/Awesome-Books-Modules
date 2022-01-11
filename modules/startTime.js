d0f84fe9da4808e2e597323f761c7d18dde3d6c3
Sahar Abdel Samad <sahar.abdelsamad@gmail.com>
2022-01-11 17:06:45 +0200

    Fix local time

--- modules/startTime.js
+++ modules/startTime.js
@@ -1,7 +1,8 @@
-/* eslint-disable no-undef */
+import { DateTime } from '../node_modules/luxon/src/luxon.js';
+
 export default function startTime() {
-  const localTime = luxon.DateTime.local();
+  const localTime = DateTime.local();
   const timeNow = document.querySelector('.time');
-  timeNow.innerText = `${localTime.toLocaleString(luxon.DateTime.DATE_FULL)}, ${localTime.toLocaleString(luxon.DateTime.TIME_WITH_SECONDS)}`;
+  timeNow.innerText = `${localTime.toLocaleString(DateTime.DATE_FULL)}, ${localTime.toLocaleString(DateTime.TIME_WITH_SECONDS)}`;
   setTimeout(startTime, 1000);
 }
